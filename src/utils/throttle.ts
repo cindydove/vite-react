4fb0235c4123f29614b27fce16ec9173e065751e
cindydove <dindydove@163.com>
2022-01-23 01:24:57 +0800

    feat:优化节流

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -2,20 +2,33 @@
 // 持续触发并不会执行多次
 // 到一定时间再去执行
 
+// const throttle = (func,delay)=>{
+//     let run = true
+//     let timer = null
+//    return function(){
+//         if(run){
+//             run = false
+//             timer = setTimeout(()=>{
+//                 func.apply(this, arguments)
+//                 run = true
+//                 clearTimeout(timer)
+//             },delay)
+//         }
+//
+//    }
+// }
+
 const throttle = (func,delay)=>{
-    let run = true
     let timer = null
-   return function(){
-        if(run){
-            run = false
+    return function(){
+        if(!timer){
             timer = setTimeout(()=>{
                 func.apply(this, arguments)
-                run = true
-                clearTimeout(timer)
+                timer = null
             },delay)
         }
 
-   }
+    }
 }
 
 export default throttle
