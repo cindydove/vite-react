680999daee8c382912b581e773602870d57cda63
cindydove <dindydove@163.com>
2022-01-23 23:36:31 +0800

    feat:样式

--- src/pages/Debounce/index.tsx
+++ src/pages/Debounce/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Input } from 'antd';
 import debounce from '@/utils/debounce';
 import throttle from '@/utils/throttle';
+import styles from './index.module.scss';
 
 function Debounce() {
     return (
-        <div>
+        <div className={styles.container}>
             <h3>防抖</h3>
             <Input
+                className={styles.input}
                 onChange={debounce((e: any) => {
                     console.log('dx---- e', e.target);
                 }, 1000)}
@@ -15,6 +17,7 @@ function Debounce() {
 
             <h3>节流</h3>
             <Input
+                className={styles.input}
                 onChange={throttle((e: any) => {
                     console.log('我一秒执行一次', new Date().getTime());
                 }, 1000)}
