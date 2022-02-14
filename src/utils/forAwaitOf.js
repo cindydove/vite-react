000e9392a14d18ca88d952077f9b03ef0aebbef8
丁鑫 <cindy13@sfmail.sf-express.com>
2022-02-14 19:21:06 +0800

    feat:for await of

--- src/utils/forAwaitOf.js
+++ src/utils/forAwaitOf.js
@@ -5,9 +5,19 @@ function TimeOut(time) {
         }, time);
     });
 }
+function TimeOut1(time) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            reject('失败');
+        }, time);
+    }).catch((err) => {
+        console.log('err 1', err);
+    });
+}
 
 async function test() {
-    let arr = [TimeOut(2000), TimeOut(1000), TimeOut(3000)];
+    let arr = [TimeOut(2000), TimeOut1(1000), TimeOut(3000)];
+    // for...of 同步迭代器
     for (let item of arr) {
         console.log(Date.now(), item.then(console.log));
     }
@@ -15,11 +25,15 @@ async function test() {
 // 异步迭代器(for-await-of)：循环等待每个Promise对象变为resolved状态才进入下一步。
 
 async function testAwait() {
-    let arr = [TimeOut(2000), TimeOut(1000), TimeOut(3000)];
-    for await (let item of arr) {
-        console.log(Date.now(), item);
+    let arr = [TimeOut(2000), TimeOut1(1000), TimeOut(3000)];
+    try {
+        for await (let item of arr) {
+            console.log(Date.now(), item);
+        }
+    } catch {
+        console.log(error);
     }
 }
 
-test();
-// testAwait();
+// test();
+testAwait();
