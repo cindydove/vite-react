2db7a409a229dc825ed7d0f659269a08048164dd
丁鑫 <cindy13@sfmail.sf-express.com>
2022-02-17 11:00:11 +0800

    feat:手写apply方法

--- src/utils/bind.js
+++ src/utils/bind.js
@@ -1,16 +1,29 @@
 Function.prototype.myBind = function (context, ...arg) {
-    if (typeof this !== 'function') {
-        throw Error('有点子问题啊');
-    }
     let newThis = context;
     if (typeof context === 'undefined' || typeof context === 'null') {
         newThis = window;
     }
     const fn = this;
-    return function () {
+    return function (...args) {
         console.log('newTHis', newThis, arg);
-        fn.apply(newThis, [...arg.slice(1)]);
+        fn.myApply(newThis, [...arg.slice(1), ...args]);
     };
 };
 
-function Test() {}
+Function.prototype.myApply = function (context, argList) {
+    context = context || window;
+    context.fn = this;
+    const result = context.fn(...argList);
+    delete context.fn;
+    return result;
+};
+
+const num = new Number(1);
+
+function Test(name, age) {
+    console.log('this----', this, name, age);
+}
+
+// Test.myApply(num, ['dx', 28]);
+Test.bind(num, 'dx')();
+console.log('dx---', num);
