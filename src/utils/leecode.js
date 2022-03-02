06d5552aa7a51b9dab010a9121cd4408beec761f
丁鑫 <cindy13@sfmail.sf-express.com>
2022-03-02 11:19:19 +0800

    feat:小孩分饼干

--- src/utils/leecode.js
+++ src/utils/leecode.js
@@ -71,3 +71,66 @@
 // console.log('dx---abcabcdabcdef', lengthOfLongestSubstring('abcabcdabcdef'));
 
 // todo    获取二叉树最深子节点并返回
+
+// 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。
+// 但是，每个孩子最多只能给一块饼干。对每个孩子 i ，都有一个胃口值  gi ，
+// 这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j ，都有一个尺寸 sj 。如果 sj >= gi
+// ，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，
+// 并输出这个最大数值。
+// 输入: [1,2,3], [1,1]
+//
+// 输出: 1
+//
+// 解释:
+// 你有三个孩子和两块小饼干，3个孩子的胃口值分别是：1,2,3。
+// 虽然你有两块小饼干，由于他们的尺寸都是1，你只能让胃口值是1的孩子满足。
+// 所以你应该输出1。
+// 示例 2:
+//
+// 输入: [1,2], [1,2,3]
+//
+// 输出: 2
+//
+// 解释:
+// 你有两个孩子和三块小饼干，2个孩子的胃口值分别是1,2。
+// 你拥有的饼干数量和尺寸都足以让所有孩子满足。
+// 所以你应该输出2.
+//
+
+function findContentChildren(child, cookie) {
+    const newChild = child.sort((a, b) => a - b);
+    const newCookie = cookie.sort((a, b) => a - b);
+
+    const childLen = child.length;
+    const cookieLen = cookie.length;
+
+    console.log('dx---newChild,newCookie', newChild, newCookie);
+    let childIndex = 0;
+    let cookieIndex = 0;
+    let count = 0;
+
+    // for (let i = 0; i < newChild.length; i++) {
+    //     console.log('dx-i，cookieIndex', i, cookieIndex);
+    //     if (newCookie[cookieIndex] >= newChild[i]) {
+    //         count++;
+    //         cookieIndex++;
+    //         continue;
+    //     } else {
+    //         i--;
+    //         cookieIndex++;
+    //     }
+    // }
+
+    while (childIndex < childLen && cookieIndex < cookieLen) {
+        if (newChild[childIndex] <= newCookie[cookieIndex]) {
+            cookieIndex++;
+            childIndex++;
+            count++;
+        } else {
+            cookieIndex++;
+        }
+    }
+    return count;
+}
+
+console.log('dx---findContentChildren', findContentChildren([1, 2, 4], [1, 2, 3, 4]));
