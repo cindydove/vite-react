414631ac890da7ae45d9b62b0ae096ea35783141
丁鑫 <cindy13@sfmail.sf-express.com>
2022-04-21 18:17:58 +0800

    feat：try catch 抛出异常的情况，只能跑出同步代码的异常

--- src/utils/promiseTest.js
+++ src/utils/promiseTest.js
@@ -107,53 +107,57 @@
 //     });
 
 // 限制数量、数据数组、处理函数
+//
+// function fetchApi(time, index) {
+//     return new Promise((resolve, reject) => {
+//         console.log(`执行第${index}个`);
+//         setTimeout(() => {
+//             resolve(`完成第${index}个---${time}`);
+//         }, time);
+//     });
+// }
+//
+// const fetchArr = [3000, 2000, 30, 40, 50, 60, 70, 80, 90, 100];
+//
+// function limitQueue(limit, fetchArr) {
+//     return new Promise((allResolve) => {
+//         const result = [];
+//         let index = 0;
+//         let fina = 0;
+//         for (let i = 0; i < limit; i++) {
+//             run(i);
+//         }
+//         function run(i) {
+//             new Promise((resolve) => {
+//                 const currentFetch = fetchArr[index];
+//                 resolve(fetchApi(currentFetch, index));
+//                 index++;
+//             })
+//                 .then((res) => {
+//                     result[i] = res;
+//                 })
+//                 .catch((error) => {
+//                     result[i] = error;
+//                 })
+//                 .finally(() => {
+//                     fina++;
+//                     if (index < fetchArr.length) {
+//                         run(index);
+//                     }
+//                     if (fina === fetchArr.length) {
+//                         allResolve(result);
+//                     }
+//                 });
+//         }
+//     });
+// }
+//
+// limitQueue(2, fetchArr)
+//     .then(console.log)
+//     .catch((res) => {
+//         console.log('error', res);
+//     });
 
-function fetchApi(time, index) {
-    return new Promise((resolve, reject) => {
-        console.log(`执行第${index}个`);
-        setTimeout(() => {
-            resolve(`完成第${index}个---${time}`);
-        }, time);
-    });
-}
 
-const fetchArr = [3000, 2000, 30, 40, 50, 60, 70, 80, 90, 100];
-
-function limitQueue(limit, fetchArr) {
-    return new Promise((allResolve) => {
-        const result = [];
-        let index = 0;
-        let fina = 0;
-        for (let i = 0; i < limit; i++) {
-            run(i);
-        }
-        function run(i) {
-            new Promise((resolve) => {
-                const currentFetch = fetchArr[index];
-                resolve(fetchApi(currentFetch, index));
-                index++;
-            })
-                .then((res) => {
-                    result[i] = res;
-                })
-                .catch((error) => {
-                    result[i] = error;
-                })
-                .finally(() => {
-                    fina++;
-                    if (index < fetchArr.length) {
-                        run(index);
-                    }
-                    if (fina === fetchArr.length) {
-                        allResolve(result);
-                    }
-                });
-        }
-    });
-}
-
-limitQueue(2, fetchArr)
-    .then(console.log)
-    .catch((res) => {
-        console.log('error', res);
-    });
+console.log("dx---Promise.resolve(2)",Promise.resolve(2))
+new Promise((resolve)=>{resolve(1)}).then(Promise.resolve(2)).then(res=>{console.log('dx---res',res)})
